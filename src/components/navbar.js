b9f64d0715523a561fd0246fe3a00257abbaacc8
gbenroofficial <68187993+gbenroofficial@users.noreply.github.com>
2023-01-30 20:01:27 +0000

    nav mini

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -22,92 +22,116 @@ const NavBar = () => {
   console.log(value);
 
   return (
-    <div className="h-screen w-61">
-      <div className="h-194 w-61 flex flex-wrap p-2 border-hidden border-1 border-gray-300">
-        <div className="h-18-24 w-55 mb-4-76 pb-4 pl-3 pt-6-24 border-hidden border-1 border-gray-300">
-          <div className="flex flex-wrap items-center justify-center border-hidden xl:flex border-1 w-25-6 h-7">
-            <img
-              src="/images/login_images/logo.png"
-              className="mt-2 mb-2"
-            ></img>
+    <div className="h-screen w-16 xl:w-60">
+      <div className="h-173 w-16 xl:w-60 flex flex-wrap p-2 border-hidden border-1 border-gray-300">
+        <div className="h-18-24 w-12 xl:w-56 mb-4-76 pb-4 pl-3 pt-6-24 border-hidden border-1 border-gray-300">
+          <div className="hidden xl:flex flex-wrap items-center justify-center border-hidden border-1 w-25-6 h-7">
+            <img src="/images/nav_logos/logo.png" className="mt-2 mb-2"></img>
+          </div>
+          <div className="w-8 h-7 xl:hidden">
+            <img src="/images/nav_logos/ig.png" />
           </div>
         </div>
 
-        <div className="w-55 h-155 border-hidden border-1 border-gray-300">
-          <div className="hover:bg-gray-100 h-12 w-55 rounded-full" href="#">
-            <div className="flex flex-wrap items-center h-12 w-55 border-hidden border-1 border-gray-300 my-1 p-3">
+        <div className="w-16 xl:w-56 h-155 border-hidden border-1 border-gray-300">
+          <div
+            className="hover:bg-gray-100 h-12 w-16 xl:w-56 rounded-full"
+            href="#"
+          >
+            <div className="flex flex-wrap items-center h-12 w-16 xl:w-56 border-hidden border-1 border-gray-300 my-1 p-3">
               <div className="flex items-center h-6 w-full">
-                <div className="w-6 h-6 mr-4">
+                <div className="w-6 h-6 xl:mr-4">
                   <img src="/images/nav_logos/home.png" />
                 </div>
                 <div className="hidden xl:flex h-6">Home</div>
               </div>
             </div>
           </div>
-          <div className="hover:bg-gray-100 h-12 w-55 rounded-full" href="#">
-            <div className="flex flex-wrap items-center h-12 w-55 border-hidden border-1 border-gray-300 my-1 p-3">
+          <div
+            className="hover:bg-gray-100 h-12 w-16 xl:w-56 rounded-full"
+            href="#"
+          >
+            <div className="flex flex-wrap items-center h-12 w-16 xl:w-56 border-hidden border-1 border-gray-300 my-1 p-3">
               <div className="flex items-center h-6 w-full">
-                <div className="w-6 h-6 mr-4">
+                <div className="w-6 h-6 xl:mr-4">
                   <img src="/images/nav_logos/search.png" />
                 </div>
                 <div className="hidden xl:flex h-6">Search</div>
               </div>
             </div>
           </div>
-          <div className="hover:bg-gray-100 h-12 w-55 rounded-full" href="#">
-            <div className="flex flex-wrap items-center h-12 w-55 border-hidden border-1 border-gray-300 my-1 p-3">
+          <div
+            className="hover:bg-gray-100 h-12 w-16 xl:w-56 rounded-full"
+            href="#"
+          >
+            <div className="flex flex-wrap items-center h-12 w-16 xl:w-56 border-hidden border-1 border-gray-300 my-1 p-3">
               <div className="flex items-center h-6 w-full">
-                <div className="w-6 h-6 mr-4">
+                <div className="w-6 h-6 xl:mr-4">
                   <img src="/images/nav_logos/explore.png" />
                 </div>
                 <div className="hidden xl:flex h-6">Explore</div>
               </div>
             </div>
           </div>
-          <div className="hover:bg-gray-100 h-12 w-55 rounded-full" href="#">
-            <div className="flex flex-wrap items-center h-12 w-55 border-hidden border-1 border-gray-300 my-1 p-3">
+          <div
+            className="hover:bg-gray-100 h-12 w-16 xl:w-56 rounded-full"
+            href="#"
+          >
+            <div className="flex flex-wrap items-center h-12 w-16 xl:w-56 border-hidden border-1 border-gray-300 my-1 p-3">
               <div className="flex items-center h-6 w-full">
-                <div className="w-6 h-6 mr-4">
+                <div className="w-6 h-6 xl:mr-4">
                   <img src="/images/nav_logos/reels.png" />
                 </div>
                 <div className="hidden xl:flex h-6">Reels</div>
               </div>
             </div>
           </div>
-          <div className="hover:bg-gray-100 h-12 w-55 rounded-full" href="#">
-            <div className="flex flex-wrap items-center h-12 w-55 border-hidden border-1 border-gray-300 my-1 p-3">
+          <div
+            className="hover:bg-gray-100 h-12 w-16 xl:w-56 rounded-full"
+            href="#"
+          >
+            <div className="flex flex-wrap items-center h-12 w-16 xl:w-56 border-hidden border-1 border-gray-300 my-1 p-3">
               <div className="flex items-center h-6 w-full">
-                <div className="w-6 h-6 mr-4">
+                <div className="w-6 h-6 xl:mr-4">
                   <img src="/images/nav_logos/messages.png" />
                 </div>
                 <div className="hidden xl:flex h-6">Messages</div>
               </div>
             </div>
           </div>
-          <div className="hover:bg-gray-100 h-12 w-55 rounded-full" href="#">
-            <div className="flex flex-wrap items-center h-12 w-55 border-hidden border-1 border-gray-300 my-1 p-3">
+          <div
+            className="hover:bg-gray-100 h-12 w-16 xl:w-56 rounded-full"
+            href="#"
+          >
+            <div className="flex flex-wrap items-center h-12 w-16 xl:w-56 border-hidden border-1 border-gray-300 my-1 p-3">
               <div className="flex items-center h-6 w-full">
-                <div className="w-6 h-6 mr-4">
+                <div className="w-6 h-6 xl:mr-4">
                   <img src="/images/nav_logos/notifications.png" />
                 </div>
                 <div className="hidden xl:flex h-6">Notifications</div>
               </div>
             </div>
           </div>
-          <div className="hover:bg-gray-100 h-12 w-55 rounded-full" href="#">
-            <div className="flex flex-wrap items-center h-12 w-55 border-hidden border-1 border-gray-300 my-1 p-3">
+          <div
+            className="hover:bg-gray-100 h-12 w-16 xl:w-56 rounded-full"
+            href="#"
+          >
+            <div className="flex flex-wrap items-center h-12 w-16 xl:w-56 border-hidden border-1 border-gray-300 my-1 p-3">
               <div className="flex items-center h-6 w-full">
-                <div className="w-6 h-6 mr-4">
+                <div className="w-6 h-6 xl:mr-4">
                   <img src="/images/nav_logos/create.png" />
                 </div>
                 <div className="hidden xl:flex h-6">Create</div>
               </div>
             </div>
           </div>
-          <div className="hover:bg-gray-100 h-12 w-55 rounded-full" href="#">
-            <div className="flex flex-wrap items-center h-12 w-55 border-hidden border-1 border-gray-300 my-1 p-3">
+          <div
+            className="hover:bg-gray-100 h-12 w-16 xl:w-56 rounded-full"
+            href="#"
+          >
+            <div className="flex flex-wrap items-center h-12 w-16 xl:w-56 border-hidden border-1 border-gray-300 my-1 p-3">
               <div className="flex items-center h-6 w-full">
-                <div className="w-6 h-6 mr-4">
+                <div className="w-6 h-6 xl:mr-4">
                   <img />
                 </div>
                 <div className="hidden xl:flex h-6">Profile</div>
@@ -116,11 +140,11 @@ const NavBar = () => {
           </div>
         </div>
 
-        <div className="flex items-center w-55 h-12 border-solid border-1 border-gray-300 my-1 p-3">
-          <div className="w-6 h-6 mr-4">
+        <div className="flex items-center w-16 xl:w-56 h-12 border-hidden border-1 border-gray-300 my-1 p-3">
+          <div className="w-6 h-6 xl:mr-4">
             <img src="/images/nav_logos/more.png" />
           </div>
-          <div className="">more</div>
+          <div className="hidden xl:flex h-6">More</div>
         </div>
       </div>
       <button
--- src/pages/navscreen.js
+++ src/pages/navscreen.js
@@ -1,6 +1,6 @@
 import { React, useEffect } from "react";
-import "../styles/app.css";
 import "../styles/sub.css";
+import "../styles/app.css";
 // import Home from "../components/home";
 import NavBar from "../components/navbar";
 import { useContext } from "react";
@@ -19,7 +19,7 @@ const NavScreen = () => {
   }
   return (
     <div className="flex flex-wrap flex-row justify-center w-vw items-start bg-gray-50">
-      <div className="w-61 h-screen border-solid border-1 border-gray-300">
+      <div className="w-16 xl:w-60 h-screen border-solid border-1 border-gray-300">
         <NavBar />
       </div>
       <div className="flex-grow h-auto border-solid border-1 border-gray-300"></div>
