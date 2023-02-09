007c64e6d6e153705c80d51c5943fe1c16907425
gbenroofficial <68187993+gbenroofficial@users.noreply.github.com>
2023-02-09 06:54:58 +0000

    corrected getUserbyid bug
    placed the function getuserbyid in useeffect so it runs after render not before.

--- src/components/cardDeck.js
+++ src/components/cardDeck.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Card from "./card";
+import { useEffect } from "react";
 
 // import { onAuthStateChanged, getAuth } from "firebase/auth";
 import { GetUserbyid } from "../services/actions";
 
 const CardDeck = () => {
   // const auth = getAuth();
-
-  GetUserbyid("2");
+  useEffect(() => {
+    const userData = GetUserbyid("2");
+    console.log(userData);
+  });
 
   return (
     <div className="w-[470px]">
--- src/services/actions.js
+++ src/services/actions.js
@@ -2,11 +2,13 @@ import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from "../lib/firebase";
 
 export async function GetUserbyid(userid) {
-  const q = await query(collection(db, "users"), where("userId", "==", userid));
-  const users = getDocs(q);
-  (await users).forEach((doc) => {
-    console.log(doc.data());
-  });
+  const q = query(collection(db, "users"), where("userId", "==", userid));
+  const users = await getDocs(q);
 
-  return users;
+  const userData = users.docs.map((item) => ({
+    ...item.data(),
+    DocId: item.id,
+  }));
+
+  return userData;
 }
